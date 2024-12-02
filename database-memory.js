5cffa3ad75c06336ed1aad9f1a4d3f70ee2d8355
CarlosFeliponi <carlosdmf.42@gmail.com>
2024-12-01 22:27:25 -0300

    Implement in-memory video database with CRUD operations using Fastify
    - Added DatabaseMemory class to handle video storage and retrieval.
    - Implemented endpoints for creating, reading, updating, and deleting videos.
    - Integrated search functionality for video retrieval.
    - Set up Fastify server to listen on port 3333.
    - Provided example requests for testing API endpoints with Rest Client.

---
+++ database-memory.js
@@ -0,0 +1,38 @@
+import { randomUUID } from "node:crypto"
+
+export class DatabaseMemory {
+    #videos = new Map()
+
+    create(video) {
+        const videoId = randomUUID()
+        this.#videos.set(videoId, video)
+    }
+
+    read(search) {
+        return Array.from(this.#videos.entries())
+            .map((videoArr) => {
+                const id = videoArr[0]
+                const data = videoArr[1]
+
+                return {
+                    id,
+                    ...data
+                }
+            })
+            .filter(video => {
+                if(search) {
+                    return video.title.includes(search)
+                }
+
+                return true
+            })
+    }
+
+    update(id, video) {
+        this.#videos.set(id, video)
+    }
+
+    delete(id) {
+        this.#videos.delete(id)
+    }
+}
